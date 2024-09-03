a845c44f30273958564708e7b69330fcca3d959d
Ata Etgi <ata.etgi@boyner.com.tr>
2024-09-03 15:23:01 +0300

    update spm

--- Package.swift
+++ Package.swift
@@ -4,23 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "segmentify-ios-sdk",
+    name: "Segmentify",
     platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "segmentify-ios-sdk",
-            targets: ["segmentify-ios-sdk"]),
+            name: "Segmentify",
+            targets: ["Segmentify"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "segmentify-ios-sdk",
-            path: "Pod/Classes"),
-        .testTarget(
-            name: "segmentify-ios-sdkTests",
-            dependencies: ["segmentify-ios-sdk"],
-            path: "Tests"),
+            name: "Segmentify",
+            path: "Pod/Classes")
     ]
 )
